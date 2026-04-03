6aa68ee78176182a290a80409dbeaa3e319fdf15
Jake Pacia <christienjakedavidpacia@gmail.com>
2026-04-03 14:35:39 +0800

    Initial Commit

---
+++ Plano/PlanoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanoApp.swift
+//  Plano
+//
+//  Created by Christien Jake Pacia on 4/3/26.
+//
+
+import SwiftUI
+
+@main
+struct PlanoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
